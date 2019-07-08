344407fecaa03f3ce6daf4e994b979756ad122be
Helena Richie <helenarichie@gmail.com>
2019-07-08 12:49:10 -0400

    Fix write_net_counts.

--- SIA_II/Calibration/perform_photometry.py
+++ SIA_II/Calibration/perform_photometry.py
@@ -287,6 +287,7 @@ def write_net_counts(dirtarget, fil, date, comp_aper_sums, aper_sum,
         header_str = '#TARGET NAME,DATE,TARGET COUNTS,ERR,FILTER,(C1,...,C{}) COUNTS,CHECK LABEL,CHECK COUNTS,REFERENCE LABEL,REF COUNTS,AIRMASS\n'.format(comp_n)
         f.write(header_str)
         comp_sums = list(zip(*comp_aper_sums))
+        print(comp_sums)
         for n, (date_i, tsum, err, csum, rsum, alt) in enumerate(zip(date_obs,
                                                                      aper_sum[0],
                                                                      t_err,
