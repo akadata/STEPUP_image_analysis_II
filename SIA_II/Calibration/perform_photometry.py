3467bd876ee6b48bd9b5fc720139e1348e4b4b63
Helena Richie <helenarichie@gmail.com>
2019-07-09 10:09:54 -0400

    Troubelshoot get_counts error.

--- SIA_II/Calibration/perform_photometry.py
+++ SIA_II/Calibration/perform_photometry.py
@@ -611,6 +611,8 @@ def get_counts(dirtarget, rightascension, declination, fil, set_rad, aper_rad,
             star = image_array[(py_int - 14):(py_int + 16),
                                (px_int - 14):(px_int + 16)]
 
+            print(star)
+
             # Ensure that the square is entirely on the image.
             if ((py_int - 14) < 0) or ((py_int + 16) > 2084):
                 continue
